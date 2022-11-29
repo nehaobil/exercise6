66c4af666d896a2ffa37ddea9544d4768dd35624
nehaobil <99745492+nehaobil@users.noreply.github.com>
2022-11-29 09:19:17 -0500

    login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,21 +1,55 @@
-import React, { useEffect } from "react";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import React, { useCallback, useEffect, useState } from "react";
 import { useNavigate } from "react-router";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 import Header from "../components/Header";
 import LoginForm from "../components/LoginForm";
 
 function LoginPage({isLoggedIn, setIsLoggedIn, setUserInformation}){
+    const [errors,setErrors] = useState();
     const navigate = useNavigate();
 
     useEffect(() => {
         if (isLoggedIn) navigate('/');
     }, [isLoggedIn]);
 
+    const loginUser = useCallback((e) => {
+        e.preventDefault();
+    }, []);
+    
+    const signUpUser = useCallback(
+        (e) => {
+            e.preventDefault();
+            const email = e.currentTarget.email.value;
+            const password = e.currentTarget.password.value;
+
+            const auth = getAuth();
+            signInWithEmailAndPassword(auth, email, password)
+                .then((userCredential) => {
+                    const user = userCredential.user;
+                    setIsLoggedIn(true);
+                    setUserInformation({
+                        email: user.email,
+                        displayName: user.displayName,
+                        uid: user.uid,
+                        accessToken: user.accessToken,
+                    });
+                })
+                .catch((error) => {
+                    const errorCode = error.code;
+                    const errorMessage = error.message;
+                    console.warn({error, errorCode, errorMessage});
+                    setErrors(errorMessage);
+                });
+    }, [setIsLoggedIn, setUserInformation]);
+         
+
     return(
     <>
     <Header setIsLoggedIn={setIsLoggedIn} setUserInformation={setUserInformation}/>    
     <div className="PageWrapper">
         <h1>Login</h1>
-        <LoginForm/>
+        <LoginForm loginUser = {loginUser}/>
     </div>
     </>
     );
